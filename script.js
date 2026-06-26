740632db4818c1a0b4cc325cf536ebad043ee742
dowasmohammed15-hub <dowasmohammed15@gmail.com>
2026-06-26 20:10:39 +0000

    dowas is doing well

--- script.js
+++
@@ -1,9 +0,0 @@
-function openMenu() {
-    const sidebar = document.getElementById("sidebar");
-    if (sidebar.style.width === "250px") {
-        sidebar.style.width = "0";
-    } else {
-        sidebar.style.width = "250px";
-    }
-
-}
